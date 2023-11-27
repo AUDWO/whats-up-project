238327a41cabecc0c379b41d56121159e755b126
audwo <kmjstj3@naver.com>
2023-11-27 22:22:04 +0900

    Update

--- prototype-client/src/App.js
+++ prototype-client/src/App.js
@@ -42,34 +42,34 @@ function App() {
   );
 
   return (
-    //<QueryClientProvider client={queryClient}>
     <BrowserRouter>
-      <GlobalStyles />
-      {PostModalOpen && <MakePostModalCp />}
-      {StoryModalOpen && <MakeStoryModalCp />}
-      {ProfileConfigModalOpen && <ProfileConfigModal />}
-      {ContentConfigModalOpen && <ProfileContentConfigModalCp />}
-      <Routes>
-        <Route path="/" element={<PageWrapper />}>
-          <Route index element={<Login />} />
-          <Route path="join" element={<SignUp />} />
-          <Route path="more-diary/:diaryId" element={<MoreDiary />} />
-          <Route path="more-story/:storyId" element={<MoreStory />} />
-          <Route path="home" element={<Home />} />
-          <Route path="/dashboard" element={<DashboardWrapper />}>
-            <Route path="diary" element={<Diary />} />
-            <Route path="make-diary" element={<MakeDiary />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="make-post" element={<MakePostModalCp />} />
-            <Route
-              path="profile/:userNickname/:otherUserId"
-              element={<Profile />}
-            />
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        {PostModalOpen && <MakePostModalCp />}
+        {StoryModalOpen && <MakeStoryModalCp />}
+        {ProfileConfigModalOpen && <ProfileConfigModal />}
+        {ContentConfigModalOpen && <ProfileContentConfigModalCp />}
+        <Routes>
+          <Route path="/" element={<PageWrapper />}>
+            <Route index element={<Login />} />
+            <Route path="join" element={<SignUp />} />
+            <Route path="more-diary/:diaryId" element={<MoreDiary />} />
+            <Route path="more-story/:storyId" element={<MoreStory />} />
+            <Route path="home" element={<Home />} />
+            <Route path="/dashboard" element={<DashboardWrapper />}>
+              <Route path="diary" element={<Diary />} />
+              <Route path="make-diary" element={<MakeDiary />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="make-post" element={<MakePostModalCp />} />
+              <Route
+                path="profile/:userNickname/:otherUserId"
+                element={<Profile />}
+              />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
-    //</QueryClientProvider>
   );
 }
 

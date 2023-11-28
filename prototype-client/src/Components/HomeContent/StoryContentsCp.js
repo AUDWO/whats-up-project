fd81662015734cf31254391900c00fd688fcc699
audwo <kmjstj3@naver.com>
2023-11-28 10:32:19 +0900

    Update

--- prototype-client/src/Components/HomeContent/StoryContentsCp.js
+++ prototype-client/src/Components/HomeContent/StoryContentsCp.js
@@ -21,6 +21,9 @@ import StoryContentCp from "./StoryContentCp";
 //Atom
 import ModalOpenAtom from "../../store/ModalOpenAtom";
 import stateUpdateAtom from "../../store/stateUpdateAtom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const StoryContentsCp = () => {
   const storyUpdate = useRecoilValue(stateUpdateAtom("story"));
@@ -44,41 +47,41 @@ const StoryContentsCp = () => {
 
   //fetchStoryies();
 
-  useEffect(() => {
+  /* useEffect(() => {
     setStoryModalOpen(false);
     //fetchStoryies();
   }, [storyUpdate]);
-
+*/
   console.log("data");
   console.log(data);
   console.log("data");
 
   return (
-    //<QueryClientProvider client={queryClient}>
-    <StoryWrapper>
-      <StoryContents>
-        <MakeStoryContent
-          onClick={() => {
-            setStoryModalOpen(!storyModalOpen);
-          }}
-        >
-          <StoryProfile>
-            <MakeStoryProfileImg>
-              <PlusIcon />
-            </MakeStoryProfileImg>
-            <StoryProfileName>Make story</StoryProfileName>
-          </StoryProfile>
-        </MakeStoryContent>
-        {stories.map((story) => {
-          return (
-            <Link to={`/more-story/${story.id}`} key={story.id}>
-              <StoryContentCp index={story.id} story={story} />
-            </Link>
-          );
-        })}
-      </StoryContents>
-    </StoryWrapper>
-    //</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <StoryWrapper>
+        <StoryContents>
+          <MakeStoryContent
+            onClick={() => {
+              setStoryModalOpen(!storyModalOpen);
+            }}
+          >
+            <StoryProfile>
+              <MakeStoryProfileImg>
+                <PlusIcon />
+              </MakeStoryProfileImg>
+              <StoryProfileName>Make story</StoryProfileName>
+            </StoryProfile>
+          </MakeStoryContent>
+          {stories.map((story) => {
+            return (
+              <Link to={`/more-story/${story.id}`} key={story.id}>
+                <StoryContentCp index={story.id} story={story} />
+              </Link>
+            );
+          })}
+        </StoryContents>
+      </StoryWrapper>
+    </QueryClientProvider>
   );
 };
 
--- prototype-client/src/index.js
+++ prototype-client/src/index.js
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import App from "./App";
 import { RecoilRoot } from "recoil";
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </RecoilRoot>
 );

57d603f79c9c73c26f12cf729138bbe774e9af46
audwo <kmjstj3@naver.com>
2023-12-02 23:17:25 +0900

    Create UserInfoContext

--- prototype-client/src/contextApi/UserInfoProvider.js
+++ prototype-client/src/contextApi/UserInfoProvider.js
@@ -1,5 +1,36 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
+import axios from "axios";
 
-const userInfoConteext = createContext();
+const UserInfoContext = createContext();
 
-export const UserInfoProvider = () => {};
+export const UserInfoProvider = ({ children }) => {
+  const [userInfo, setUserInfo] = useState({});
+
+  useEffect(() => {
+    const response = fetchuserInfoData();
+    setUserInfo({ ...response.data });
+  }, []);
+
+  const fetchuserInfoData = async () => {
+    try {
+      const response = await axios.get("/page/user-info");
+      return response;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <UserInfoContext.Provider value={userInfo}>
+      {children}
+    </UserInfoContext.Provider>
+  );
+};
+
+export function useUserInfoValue() {
+  const value = useContext(UserInfoContext);
+  if (value === undefined) {
+    throw new Error("useUserInfoValue should be used within UserInfoProvider ");
+  }
+  return value;
+}

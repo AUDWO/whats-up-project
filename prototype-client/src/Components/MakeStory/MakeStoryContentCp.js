f534c0b1c67d74315c255b15049ddab99722d0c5
audwo <kmjstj3@naver.com>
2023-12-03 20:59:35 +0900

    Update

--- prototype-client/src/Components/MakeStory/MakeStoryContentCp.js
+++ prototype-client/src/Components/MakeStory/MakeStoryContentCp.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+/*import React, { useState } from "react";
 import axios from "axios";
 import { useRecoilState, useRecoilValue } from "recoil";
 
@@ -12,17 +12,19 @@ import {
   MakeStoryFormWrapper,
   MakeStoryCancelButton,
   MakeStoryButton,
-} from "../../StyledComponents/MakeStoryStyle/MakeStoryContentStyle";
+} from "../../StyledComponents/MakeStoryStyle/MakeStoryContentStyle";*/
 
 //Atoms
+/*
 import postImgAtom from "../../store/PostImgAtom";
 import stateUpdateAtom from "../../store/stateUpdateAtom";
 import ModalOpenAtom from "../../store/ModalOpenAtom";
-import userInfoAtom from "../../store/userState/userAtom";
+import userInfoAtom from "../../store/userState/userAtom";*/
 
 //Queries
 //import { useQueryClient, useMutation } from "@tanstack/react-query";
 
+/*
 const MakeStoryContentCp = () => {
   const [StoryModalOpen, setStoryModalOpen] = useRecoilState(
     ModalOpenAtom("makeStoryModal")
@@ -36,8 +38,9 @@ const MakeStoryContentCp = () => {
 
   const formData = new FormData();
   formData.append("img", storyImgUrl);
-  const userInfo = useRecoilValue(userInfoAtom);
+  const userInfo = useRecoilValue(userInfoAtom);*/
 
+/*
   const handlePostStory2 = async () => {
     if (storyImgUrl) {
       try {
@@ -52,8 +55,9 @@ const MakeStoryContentCp = () => {
       }
     }
     if (!storyImgUrl) alert("사진을 선택해주세요.");
-  };
+  };*/
 
+/*
   const handlePostStory = async () => {
     if (storyImgUrl) {
       try {
@@ -72,9 +76,9 @@ const MakeStoryContentCp = () => {
         console.error("게시 중 오류 발생:", error);
       }
     }
-  };
+  };*/
 
-  /*
+/*
   const handle = () => {
     setStoryUdate(!storyUpdate);
   };
@@ -82,7 +86,7 @@ const MakeStoryContentCp = () => {
   
   */
 
-  /*
+/*
 
   const handleSubmitPostStoryImg = async () => {
     console.log("handleSubmitPostStoryImg시작");
@@ -125,7 +129,7 @@ const MakeStoryContentCp = () => {
     if (!storyImgUrl) alert("사진을 선택해주세요.");
   };*/
 
-  /*
+/*
   const { mutate } = useMutation(
     // 비동기 함수 정의
     async () => {
@@ -162,6 +166,7 @@ const MakeStoryContentCp = () => {
     mutate();
   };*/
 
+/*
   return (
     <MakeStoryContentWrapper>
       <MakeStoryProfileWrapper>
@@ -181,6 +186,90 @@ const MakeStoryContentCp = () => {
             //handleButtonClick();
             handlePostStory();
 
+            setStoryModalOpen(!StoryModalOpen);
+            setStoryImgUrl(null);
+          }}
+        >
+          게시하기
+        </MakeStoryButton>
+        <MakeStoryCancelButton onClick={() => {}}>
+          삭제하기
+        </MakeStoryCancelButton>
+      </MakeStoryFormWrapper>
+    </MakeStoryContentWrapper>
+  );
+};*/
+
+import React, { useState } from "react";
+import axios from "axios";
+import { useRecoilState } from "recoil";
+
+//Styled-Componeny
+import {
+  MakeStoryContent,
+  MakeStoryContentWrapper,
+  MakeStoryProfileImg,
+  MakeStoryProfileWrapper,
+  MakeStoryNickname,
+  MakeStoryFormWrapper,
+  MakeStoryCancelButton,
+  MakeStoryButton,
+} from "../../StyledComponents/MakeStoryStyle/MakeStoryContentStyle";
+
+//Atoms
+import postImgAtom from "../../store/PostImgAtom";
+import stateUpdateAtom from "../../store/stateUpdateAtom";
+import ModalOpenAtom from "../../store/ModalOpenAtom";
+
+const MakeStoryContentCp = () => {
+  const [StoryModalOpen, setStoryModalOpen] = useRecoilState(
+    ModalOpenAtom("makeStoryModal")
+  );
+  const [storyUpdate, setStoryUdate] = useRecoilState(stateUpdateAtom("story"));
+  const [storyImgUrl, setStoryImgUrl] = useRecoilState(postImgAtom("storyImg"));
+
+  const [content, setContent] = useState(null);
+
+  const formData = new FormData();
+  formData.append("img", storyImgUrl);
+
+  const handlePostStory = async () => {
+    if (storyImgUrl) {
+      try {
+        const imgData = await axios.post("/post/storyimg", formData);
+
+        const response = await axios.post("/post/story", {
+          content: content,
+          url: imgData.data.url,
+        });
+
+        const handle = () => {
+          setStoryUdate(!storyUpdate);
+        };
+        handle();
+      } catch (error) {
+        console.error("게시 중 오류 발생:", error);
+      }
+    }
+  };
+
+  return (
+    <MakeStoryContentWrapper>
+      <MakeStoryProfileWrapper>
+        <MakeStoryProfileImg></MakeStoryProfileImg>
+        <MakeStoryNickname>myeongjae_7053</MakeStoryNickname>
+      </MakeStoryProfileWrapper>
+      <MakeStoryContent
+        placeholder="내용을 입력하세요!"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      />
+      <MakeStoryFormWrapper>
+        <MakeStoryButton
+          onClick={() => {
+            handlePostStory();
             setStoryModalOpen(!StoryModalOpen);
             setStoryImgUrl(null);
           }}

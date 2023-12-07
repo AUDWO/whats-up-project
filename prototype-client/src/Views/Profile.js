ca5be6acfc3419361ccac54aa4ee2bfe1acc44ae
audwo <kmjstj3@naver.com>
2023-12-07 23:59:56 +0900

    Udpdate

--- prototype-client/src/Views/Profile.js
+++ prototype-client/src/Views/Profile.js
@@ -3,21 +3,28 @@ import HomeWraper from "../StyledComponents/HomeStyle/HomeWrapper";
 
 import ProfileCp from "../Components/Profile/ProfileCp";
 import { useParams } from "react-router-dom";
+import { UserInfoProvider } from "../contextApi/UserInfoProvider";
 
 const Profile = () => {
   const { otherUserId } = useParams();
 
   if (otherUserId) {
+    console.log("otherUserId는 존재해!!");
     return (
       <HomeWraper>
-        <ProfileCp otherUserId={otherUserId} />
+        <UserInfoProvider>
+          <ProfileCp otherUserId={otherUserId} />
+        </UserInfoProvider>
       </HomeWraper>
     );
   }
   if (!otherUserId) {
+    console.log("otherUserId는 존재하지 않아!!!");
     return (
       <HomeWraper>
-        <ProfileCp />
+        <UserInfoProvider>
+          <ProfileCp />
+        </UserInfoProvider>
       </HomeWraper>
     );
   }

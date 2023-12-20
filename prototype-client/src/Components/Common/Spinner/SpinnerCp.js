217a1ed275663542b27dea9e307c964c11e962c4
audwo <kmjstj3@naver.com>
2023-12-20 23:26:13 +0900

    Update Spinner Component

---
+++ prototype-client/src/Components/Common/Spinner/SpinnerCp.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ClipLoader } from "react-spinners";
+
+const SpinnerCp = ({ color, size }) => {
+  return <ClipLoader color={color} size={size} />;
+};
+
+export default SpinnerCp;

3b89caa7a4076ef0ef1f2e952bb2a2aad1c031ef
audwo <kmjstj3@naver.com>
2023-12-31 18:13:39 +0900

    Update

--- controllers/page.js
+++ controllers/page.js
@@ -181,10 +181,11 @@ exports.renderAllPost = async (req, res, next) => {
         const output = await blurhashFromURL(postImgUrl);
         return output;
       };
+      /*
       posts = posts.map(async (post) => {
         const blurhashedImg = await getBlurhash(post.img);
         return { ...post, hash: blurhashedImg };
-      });
+      });*/
 
       const currentCount = posts.length;
 

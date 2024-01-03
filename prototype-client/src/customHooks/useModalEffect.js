dd7215fcacbd37d0c62f3840fe78faa0712d126e
audwo <kmjstj3@naver.com>
2024-01-03 16:25:45 +0900

    Update 내부로직

--- prototype-client/src/customHooks/useModalEffect.js
+++ prototype-client/src/customHooks/useModalEffect.js
@@ -1,20 +1,17 @@
 import { useEffect } from "react";
 
-const useModalOutClickEffect = (refs, callback) => {
+const useModalOutClickEffect = (modalRef, callback) => {
   useEffect(() => {
     const handleClick = (e) => {
-      if (refs.every((ref) => ref && !ref.current.contains(e.target))) {
+      if (!modalRef.current.contains(e.target)) {
         callback();
       }
-      e.stopPropagation();
     };
-
     document.addEventListener("click", handleClick);
-
     return () => {
       document.removeEventListener("click", handleClick);
     };
-  }, [refs]);
+  }, []);
 };
 
 export default useModalOutClickEffect;

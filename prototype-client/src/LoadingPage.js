7487b60ce990ab030332ee0c56bd64bafe3cf7cb
audwo <kmjstj3@naver.com>
2024-01-03 16:23:31 +0900

    fallback: mainsidebar

---
+++ prototype-client/src/LoadingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainSideBarCp from "./Components/MainSideBar/MainSideBarCp";
+
+const LoadingPage = () => {
+  return (
+    <div>
+      <MainSideBarCp />
+    </div>
+  );
+};
+
+export default LoadingPage;

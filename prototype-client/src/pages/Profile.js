d2693bf2c78a2eed7fda84f2b220e7feecbd7504
audwo <kmjstj3@naver.com>
2024-01-05 15:50:11 +0900

    if문 제거

--- prototype-client/src/pages/Profile.js
+++ prototype-client/src/pages/Profile.js
@@ -8,26 +8,13 @@ import { UserInfoProvider } from "../contextApi/UserInfoProvider";
 const Profile = () => {
   const { otherUserId } = useParams();
 
-  if (otherUserId) {
-    console.log("otherUserId는 존재해!!");
-    return (
-      <HomeWraper>
-        <UserInfoProvider>
-          <ProfileCp otherUserId={otherUserId} />
-        </UserInfoProvider>
-      </HomeWraper>
-    );
-  }
-  if (!otherUserId) {
-    console.log("otherUserId는 존재하지 않아!!!");
-    return (
-      <HomeWraper>
-        <UserInfoProvider>
-          <ProfileCp />
-        </UserInfoProvider>
-      </HomeWraper>
-    );
-  }
+  return (
+    <HomeWraper>
+      <UserInfoProvider>
+        <ProfileCp otherUserId={otherUserId} />
+      </UserInfoProvider>
+    </HomeWraper>
+  );
 };
 
 export default Profile;

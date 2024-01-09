e3f8fe0898a371babf0a7bea50a3929a999a823a
audwo <kmjstj3@naver.com>
2024-01-09 14:23:10 +0900

    update

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const {
 const { findUserById } = require("../controllers/user");
 
 router.get("/:id", isLoggedIn, findUserById);
-router.get("/find-follower/:userId", renderFollowInfo);
+router.get("/follow-info/:userId", renderFollowInfo);
 router.get("/check-follower/:userId", checkFollower);
 router.post("/:id/follow", isLoggedIn, follow);
 router.post("/:id/unfollow", isLoggedIn, unfollow);
